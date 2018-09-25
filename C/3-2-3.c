57a5f0ba057f438ba07a8baf313fbe8836752804
chika274 <chika1281clover@docomo.ne.jp>
2018-09-25 12:41:49 +0900

    修正

--- C/3-2-3.c
+++ C/3-2-3.c
@@ -3,12 +3,13 @@
 int main(void)
 {
 	float x = 0.0;
+	int i=0;
 	while(1) {
-		x += 0.1;
-
+		i += 1;
+		x = 0.1*i;
 		printf("x = %lf\n", x);
 
-		if (x >= 1.0) 
+		if (x == 1.0) 
 			break;  
 	}
 
@@ -17,6 +18,6 @@ int main(void)
 
 /*
 
-== を >= に変えて許容範囲を広げる。
+インクリメントは整数で行う。
 
 */
